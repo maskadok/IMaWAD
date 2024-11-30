895d04dc5aa921a487a75784be0b29e65d54478a
maskadok <127107139+maskadok@users.noreply.github.com>
2024-11-30 21:01:11 +0500

    Update script.js

--- Assignment 7/script.js
+++ Assignment 7/script.js
@@ -2,10 +2,12 @@ let savedHeroes = [];
 let url = "https://akabab.github.io/superhero-api/api/all.json";
 let heroImage = document.getElementById("hero-avatar");
 let heroTitle = document.getElementById("hero-title");
-let heroDescription = document.getElementById("hero-description");
+let heroFirstAppearance = document.getElementById("hero-firstAppearance");
 let heroGender = document.getElementById("hero-gender");
 let heroRace = document.getElementById("hero-race");
 let heroHeight = document.getElementById("hero-height");
+let heroWeight = document.getElementById("hero-weight");
+let heroEyeColor = document.getElementById("hero-eyeColor");
 let heroInt = document.getElementById("hero-intelligence");
 let heroStr = document.getElementById("hero-strength");
 let heroSpeed = document.getElementById("hero-speed");
@@ -21,12 +23,14 @@ async function fetchAllHeroes(){
         let randomIndex = Math.floor(Math.random() * heroes.length);
         let firstHero = heroes[randomIndex];
         console.log(firstHero);
-        
+
         heroTitle.innerText = firstHero.name;
-        heroDescription.innerText ="Description: " + firstHero.biography.firstAppearance;
+        heroFirstAppearance.innerText ="First appearance: " + firstHero.biography.firstAppearance;
         heroGender.innerText = "Gender: " + firstHero.appearance.gender;
         heroRace.innerText = "Race: " + firstHero.appearance.race;
         heroHeight.innerText = "Height: " + firstHero.appearance.height;
+        heroWeight.innerText = "Weight: " + firstHero.appearance.weight;
+        heroEyeColor.innerText = "Eye color: " + firstHero.appearance.eyeColor;
         heroInt.style.width = firstHero.powerstats.intelligence + "%";
         document.getElementById("intelligence-value").innerText = firstHero.powerstats.intelligence;
         heroStr.style.width = firstHero.powerstats.strength + "%";
@@ -39,7 +43,7 @@ async function fetchAllHeroes(){
         document.getElementById("power-value").innerText = firstHero.powerstats.power;
         heroCombat.style.width = firstHero.powerstats.combat + "%";
         document.getElementById("combat-value").innerText = firstHero.powerstats.combat;
-        heroImage.setAttribute("src", firstHero.images.md); //
+        heroImage.setAttribute("src", firstHero.images.md);
     }
     catch(error){
         console.log(`Getting some error: ${error}`);
